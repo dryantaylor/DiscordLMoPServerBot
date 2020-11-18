bfcbfbe9a66906b5d6acde8fae02ad0aebab4d76
dyrantaylor <72103204+dyrantaylor@users.noreply.github.com>
2020-11-18 19:23:34 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
 import discord
 from discord.ext import commands
-import praw
-import multiprocessing
-from cogs.redditCommands.code import load_reddit_API_info
-import time
+from multiprocessing import Process
+from reddit_image_updater import update_pictures
 import os
-
-def update_pictures():
-    perSubPictureTotal = 51 #for some reason i need to do one more than i actually want, i dont know why
-    #              (subname , link store file)
-    subs = [("cats", "cat_pictures.txt"), ("dogpictures", "dog_pictures.txt"), ("snakes", "snake_pictures.txt"),
-            ("rabbits", "bunny_pictures.txt")]
-
-    while True:
-        for subreddit,destination in subs:
-            client_id, client_secret, user_agent = load_reddit_API_info()
-            reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
-            pictures = []
-            x = 0
-            for subbmission in reddit.subreddit(subreddit).hot(limit=1000):
-                if subbmission.url.split(".")[-1] in ["png", "jpg", "jpeg"]:
-                    pictures.append(subbmission.url)
-                    x += 1
-                if x == perSubPictureTotal:
-                    break
-
-                with open(f"./cogs/redditCommands/{destination}", "w") as file:
-                    string = ""
-                    for url in pictures:
-                        string += f"{url}\n"
-                    file.write(string)
-
-                #  24hours = 86400 seconds
-        time.sleep(86400)
 
 prefix = "/"
 intents = discord.Intents.default()
@@ -45,7 +15,7 @@ async def on_ready():
     print(f"bot online as {bot.user.name}")
 
 for folderName in os.listdir("./cogs"):
-    if "code.py" in os.listdir(f"./cogs/{folderName}"):
+    if not folderName.endswith(".py") and "code.py" in os.listdir(f"./cogs/{folderName}"):
         bot.load_extension(f"cogs.{folderName}.code")
 #bot.load_extension("cogs.redditCommands.code")
 
@@ -59,7 +29,7 @@ async def qauck(ctx):
     await ctx.send("QAUCK QAUCK")
 
 if __name__ == "__main__":
-    file_updater = multiprocessing.Process(target=update_pictures)
-    file_updater.start()
+    job = Process(target=update_pictures)
+    job.start()
     bot.run(secret)
-    file_updater.join()
+    job.join()
