014c999c26dabf468a3f3e9a7b82f5f3c06d89cd
dyrantaylor <72103204+dyrantaylor@users.noreply.github.com>
2020-12-28 15:21:18 +0000

    Functions Across multiple servers

--- cogs/LevelSystem/code.py
+++ cogs/LevelSystem/code.py
@@ -19,26 +19,34 @@ class LevelSystem(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if member.id not in self.exp_table.keys():
-            self.exp_table[str(member.id)] = [0,0,0]
+        if member.id not in self.exp_table[member.guild.id].keys():
+            self.exp_table[str(member.guild.id)][str(member.id)] = [0,0,0]
             with open(self.fileLoc, "w") as file:
                 json.dump(self.exp_table, file)
 
+    @commands.Cog.listener()
+    async def on_guild_join(self,guild):
+        print("hello aaaa")
+        self.exp_table[str(guild.id)] = {}
+        for member in guild.members:
+            if member.id not in self.exp_table[str(guild.id)].keys() and not member.bot:
+                self.exp_table[str(guild.id)][str(member.id)] = [0, 0, 0]
 
+        with open(self.fileLoc,"w") as file:
+            json.dump(self.exp_table,file)
 
     @commands.Cog.listener()
     async def on_message(self,message):
-
         if not message.author.bot:
+            guild_id = str(message.guild.id)
             try:
                 minTimeBetween = 180 #3 mins in seconds
                 currTime = int(time.time())
-                if currTime - self.exp_table[str(message.author.id)][0] > minTimeBetween:
-                    exp = self.exp_table[str(message.author.id)][1]
-                    level = self.exp_table[str(message.author.id)][2]
-                    self.exp_table[str(message.author.id)][0] = currTime #time since last exp gain
+                if currTime - self.exp_table[guild_id][str(message.author.id)][0] > minTimeBetween:
+                    exp = self.exp_table[guild_id][str(message.author.id)][1]
+                    level = self.exp_table[guild_id][str(message.author.id)][2]
+                    self.exp_table[guild_id][str(message.author.id)][0] = currTime #time since last exp gain
                     exp += random.randint(20,150) #exp in that level
-
                     changed = False
                     while True:
                         next_level_needed = self.calc_exp_to_next_level(level)
@@ -51,8 +59,8 @@ class LevelSystem(commands.Cog):
                     if changed:
                         await message.channel.send(f"{message.author.mention} leveled up\n```Level: {level} exp:\n{self.calc_exp_bar_draw([currTime,exp,level])} {exp}/{self.calc_exp_to_next_level(level)}```")
 
-                    self.exp_table[str(message.author.id)][1] = exp
-                    self.exp_table[str(message.author.id)][2] = level
+                    self.exp_table[guild_id][str(message.author.id)][1] = exp
+                    self.exp_table[guild_id][str(message.author.id)][2] = level
                     with open(self.fileLoc,"w") as file:
                         json.dump(self.exp_table,file)
 
@@ -61,26 +69,14 @@ class LevelSystem(commands.Cog):
             except Exception as e:
                 print(e)
 
-    @commands.Cog.listener()
-    async def on_guild_join(self,ctx):
-        print("hello")
-        for member in ctx.members:
-            if member.id not in self.exp_table.keys() and not member.bot:
-                self.exp_table[str(member.id)] = [0, 0, 0]
-
-        with open(self.fileLoc,"w") as file:
-            json.dump(self.exp_table,file)
-
-
-
 
     @commands.command()
     async def exp(self,ctx):
-        user = self.exp_table[str(ctx.author.id)]
+        user = self.exp_table[str(ctx.guild.id)][str(ctx.author.id)]
         exp_to_next_level = self.calc_exp_to_next_level(user[2])
 
         exp_bar = self.calc_exp_bar_draw(user)
-        rank,n_users = self.calc_user_rank(str(ctx.author.id))
+        rank,n_users = self.calc_user_rank(str(ctx.guild.id),str(ctx.author.id))
         # ◕  ◔  ⬤  ○ ◐
 
         await ctx.send(f"{ctx.author.mention}\n```Level: {user[2]} rank: {rank}/{n_users}exp:\n{exp_bar} {user[1]}/{exp_to_next_level}```")
@@ -119,17 +115,15 @@ class LevelSystem(commands.Cog):
         # ◕  ◔  ⬤  ○ ◐
         return exp_bar
 
-    def calc_user_rank(self,user_id: str):
+    def calc_user_rank(self,guild_id: str,user_id: str):
 
-        user_level = int(self.exp_table[user_id][2])
-        user_exp = int(self.exp_table[user_id][1])
+        user_level = int(self.exp_table[guild_id][user_id][2])
+        user_exp = int(self.exp_table[guild_id][user_id][1])
         rank = 1
         n_users = 0
         user_id = user_id
-        print(type(user_id))
-        for user in self.exp_table.items():
+        for user in self.exp_table[guild_id].items():
             if user[0] != user_id:
-                print(user)
                 pass
                 if int(user[1][2]) > user_level or (int(user[1][2]) == user_level and int(user[1][1]) > user_exp): #fiz
                     rank+=1
